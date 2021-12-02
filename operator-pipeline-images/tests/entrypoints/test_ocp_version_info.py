ee85c6752b426d2f286100d774436938044180c1
haripate <>
2021-12-02 18:39:15 +0530

    Add json return value to the test data for ocp_version_info tests

--- operator-pipeline-images/tests/entrypoints/test_ocp_version_info.py
+++ operator-pipeline-images/tests/entrypoints/test_ocp_version_info.py
@@ -6,12 +6,16 @@ from operatorcert.entrypoints import ocp_version_info
 @patch("operatorcert.entrypoints.ocp_version_info.setup_argparser")
 @patch("operatorcert.entrypoints.ocp_version_info.pathlib.Path")
 @patch("operatorcert.entrypoints.ocp_version_info.ocp_version_info")
-@patch("operatorcert.entrypoints.ocp_version_info.json")
 def test_main(
-    mock_json: MagicMock,
     mock_version_info: MagicMock,
     mock_path: MagicMock,
     mock_arg_parser: MagicMock,
 ) -> None:
+    mock_version_info.return_value = {
+        "versions_annotation": "ocp_versions_range",
+        "max_version_property": "max_ocp_version",
+        "indices": "indices",
+        "max_version_index": "sample_index",
+    }
     ocp_version_info.main()
     mock_version_info.assert_called_once()
