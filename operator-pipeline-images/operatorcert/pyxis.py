57cad4863f2ae88802d9881668f32b4b125e1bc4
MarcinGinszt <32140673+MarcinGinszt@users.noreply.github.com>
2021-12-08 11:36:29 +0100

    Use proxy for external preprod environments (#194)
    * using proxy to access Pyxis
    
    * proxy only for external pyxis
    
    * black format
    
    * added the documentation url

--- operator-pipeline-images/operatorcert/pyxis.py
+++ operator-pipeline-images/operatorcert/pyxis.py
@@ -35,6 +35,17 @@ def _get_session() -> requests.Session:
     api_key = os.environ.get("PYXIS_API_KEY")
     cert = os.environ.get("PYXIS_CERT_PATH")
     key = os.environ.get("PYXIS_KEY_PATH")
+    env = os.environ.get("ENVIRONMENT")
+
+    # Document about the proxy configuration:
+    # https://source.redhat.com/groups/public/customer-platform-devops/digital_experience_operations_dxp_ops_wiki/using_squid_proxy_to_access_akamai_preprod_domains_over_vpn
+    proxies = {}
+    # If it's external preprod
+    if env != "prod" and api_key:
+        proxies = {
+            "http": "http://squid.corp.redhat.com:3128",
+            "https": "http://squid.corp.redhat.com:3128",
+        }
 
     # API key or cert + key need to be provided using env variable
     if not api_key and (not cert or not key):
@@ -44,12 +55,20 @@ def _get_session() -> requests.Session:
         )
 
     session = requests.Session()
+
     if api_key:
         LOGGER.debug("Pyxis session using API key is created")
         session.headers.update({"X-API-KEY": api_key})
     else:
         LOGGER.debug("Pyxis session using cert + key is created")
         session.cert = (cert, key)
+
+    if proxies:
+        LOGGER.debug(
+            "Pyxis session configured for Proxy (external preprod environment)"
+        )
+        session.proxies.update(proxies)
+
     return session
 
 
