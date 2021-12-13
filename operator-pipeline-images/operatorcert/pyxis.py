e4667e3e307d2819131e59f9459fb905244e2336
Wai Cheang <wcheang@redhat.com>
2021-12-13 14:52:17 -0500

    Update Pyxis module to also verify that cert paths exist
    With some upcoming changes for ISV-1278, the cert+key filepaths
    could be set but not point to existent file. This will detect the
    case and provide a clearer error message.

--- operator-pipeline-images/operatorcert/pyxis.py
+++ operator-pipeline-images/operatorcert/pyxis.py
@@ -47,21 +47,26 @@ def _get_session() -> requests.Session:
             "https": "http://squid.corp.redhat.com:3128",
         }
 
-    # API key or cert + key need to be provided using env variable
-    if not api_key and (not cert or not key):
-        raise Exception(
-            "No auth details provided for Pyxis. "
-            "Either define PYXIS_API_KEY or PYXIS_CERT_PATH + PYXIS_KEY_PATH"
-        )
-
     session = requests.Session()
 
     if api_key:
         LOGGER.debug("Pyxis session using API key is created")
         session.headers.update({"X-API-KEY": api_key})
+    elif cert and key:
+        if os.path.exists(cert) and os.path.exists(key):
+            LOGGER.debug("Pyxis session using cert + key is created")
+            session.cert = (cert, key)
+        else:
+            raise Exception(
+                "PYXIS_CERT_PATH or PYXIS_KEY_PATH does not point to a file that "
+                "exists."
+            )
     else:
-        LOGGER.debug("Pyxis session using cert + key is created")
-        session.cert = (cert, key)
+        # API key or cert + key need to be provided using env variable
+        raise Exception(
+            "No auth details provided for Pyxis. "
+            "Either define PYXIS_API_KEY or PYXIS_CERT_PATH + PYXIS_KEY_PATH"
+        )
 
     if proxies:
         LOGGER.debug(
