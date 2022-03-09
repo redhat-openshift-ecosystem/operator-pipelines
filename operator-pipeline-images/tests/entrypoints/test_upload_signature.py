7d06905cd08fc444ddfa7b425a3f20e198e067a3
Wai Cheang <wcheang@redhat.com>
2022-03-09 00:51:44 -0500

    Update signing tasks to support both single and multiple requests

--- operator-pipeline-images/tests/entrypoints/test_upload_signature.py
+++ operator-pipeline-images/tests/entrypoints/test_upload_signature.py
@@ -6,14 +6,21 @@ from unittest.mock import MagicMock, patch
 from operatorcert.entrypoints import upload_signature
 
 
+@patch("json.loads")
 @patch("operatorcert.entrypoints.upload_signature.setup_argparser")
 @patch("operatorcert.entrypoints.upload_signature.upload_signature")
 def test_main(
     mock_upload_signature: MagicMock,
     mock_arg_parser: MagicMock,
+    mock_json_loads: MagicMock,
 ) -> None:
+    mock_arg_parser.parse_args = MagicMock
+    mock_arg_parser.parse_args.pyxis_url = "https://test-pyxis.fake.url"
+    mock_json_loads.return_value = [{"request_id": "request_id123"}]
     upload_signature.main()
-    mock_upload_signature.assert_called_once()
+    mock_upload_signature.assert_called_once_with(
+        {"request_id": "request_id123"}, "https://test-pyxis.fake.url"
+    )
 
 
 def test_parse_repository_name():
@@ -29,47 +36,46 @@ def test_parse_repository_name():
         assert result == expected_repo
 
 
+@patch("operatorcert.entrypoints.upload_signature.parse_repository_name")
 @patch("operatorcert.entrypoints.upload_signature.pyxis.post")
-def test_upload_signature(mock_post: MagicMock) -> None:
-    args = MagicMock()
-    args.pyxis_url = "https://test-pyxis.fake.url"
-    args.manifest_digest = "sha256:123456"
-    args.reference = "registry.redhat.io/redhat/community-operator-index:v4.9"
-    args.repository = "redhat/community-operator-index"
-    args.sig_key_id = "testkeyid"
-    args.signature_data = "dGVzdHNpZ25hdHVyZWRhdGEK"
-
-    upload_signature.upload_signature(args)
+def test_upload_signature(mock_post: MagicMock, mock_repo_name: MagicMock) -> None:
+    data = {
+        "manifest_digest": "sha256:123456",
+        "docker_reference": "registry.redhat.io/redhat/community-operator-index:v4.9",
+        "sig_key_id": "testkeyid",
+        "signed_claim": "dGVzdHNpZ25hdHVyZWRhdGEK",
+    }
+    upload_signature.upload_signature(data, "https://test-pyxis.fake.url")
     mock_post.assert_called_once_with(
         "https://test-pyxis.fake.url/v1/signatures",
         {
-            "manifest_digest": args.manifest_digest,
-            "reference": args.reference,
-            "repository": args.repository,
-            "sig_key_id": args.sig_key_id,
-            "signature_data": args.signature_data,
+            "manifest_digest": data["manifest_digest"],
+            "reference": data["docker_reference"],
+            "repository": mock_repo_name.return_value,
+            "sig_key_id": data["sig_key_id"],
+            "signature_data": data["signed_claim"],
         },
     )
 
 
 @patch("operatorcert.entrypoints.upload_signature.pyxis.post")
 def test_upload_signature_http_errors(mock_post: MagicMock) -> None:
-    args = MagicMock()
-    args.pyxis_url = "https://test-pyxis.fake.url"
-    args.manifest_digest = "sha256:123456"
-    args.reference = "registry.redhat.io/redhat/community-operator-index:v4.9"
-    args.repository = "redhat/community-operator-index"
-    args.sig_key_id = "testkeyid"
-    args.signature_data = "dGVzdHNpZ25hdHVyZWRhdGEK"
+    data = {
+        "manifest_digest": "sha256:123456",
+        "docker_reference": "registry.redhat.io/redhat/community-operator-index:v4.9",
+        "sig_key_id": "testkeyid",
+        "signed_claim": "dGVzdHNpZ25hdHVyZWRhdGEK",
+    }
+    pyxis_url = "https://test-pyxis.fake.url"
 
     fake_rsp = Response()
     fake_rsp.status_code = 409
     fake_err = HTTPError(response=fake_rsp)
     mock_post.side_effect = fake_err
-    upload_signature.upload_signature(args)
+    upload_signature.upload_signature(data, pyxis_url)
 
     fake_rsp.status_code = 404
     fake_err = HTTPError(response=fake_rsp)
     mock_post.side_effect = fake_err
     with pytest.raises(HTTPError):
-        upload_signature.upload_signature(args)
+        upload_signature.upload_signature(data, pyxis_url)
