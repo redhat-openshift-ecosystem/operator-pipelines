472d70307c7c09d3ffae01a153e6d4619c08c23f
tbak <tbak@redhat.com>
2023-09-12 10:39:31 +0200

    [ISV-3982] create entrypoint for prow community

---
+++ operator-pipeline-images/tests/community_prow/test_start_community_prow.py
@@ -0,0 +1,6 @@
+from operatorcert.community_prow.start_community_prow import initiate_community_prow
+
+
+# placeholder test for initial entrypoint
+def test_initiate_community_prow():
+    assert "Hello world" == initiate_community_prow()
