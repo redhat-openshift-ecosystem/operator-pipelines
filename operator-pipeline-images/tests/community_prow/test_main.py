0c961bc8e0f43451feef87c6f8e3bbc0ec847e83
tbak <tbak@redhat.com>
2023-09-22 12:25:44 +0200

    test

--- operator-pipeline-images/tests/community_prow/test_main.py
+++ operator-pipeline-images/tests/community_prow/test_main.py
@@ -1,3 +1,12 @@
-from operatorcert.entrypoints.community_prow import main
+from operatorcert.entrypoints import community_prow
+from unittest.mock import patch, MagicMock
+from typing import Any
 
-main()
+
+def test_test_prow_logging(monkeypatch: Any) -> None:
+    oc_mock = MagicMock()
+    oc_mock.get_server_version.return_value = {"version": "1.2.3"}
+    monkeypatch.setenv("PULL_REFS", "test_ref")
+
+    with patch("operatorcert.entrypoints.community_prow.oc", oc_mock):
+        community_prow.main()
