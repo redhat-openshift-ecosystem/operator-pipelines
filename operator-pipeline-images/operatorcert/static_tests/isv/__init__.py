9df79ac4dcb610e0d0f86016f11de8692e47cdf8
mszymutk <mszymutk@redhat.com>
2023-11-06 13:56:13 +0100

    Add static checks to ISV pipeline
    The static checks have been added to the ISV hosted pipeline and new
    test for pruned graphs have been created as part of this commit.
    
    The test checks if a new bundle prunes graph and Warn users.
    
    JIRA: ISV-3998

---
+++ operator-pipeline-images/operatorcert/static_tests/isv/__init__.py
@@ -0,0 +1,8 @@
+"""
+ISV operators static test suite
+
+A test suite is a python package with two modules called "operator"
+and "bundle" each containing a collection of check functions for the
+respective type of resources.
+
+"""
