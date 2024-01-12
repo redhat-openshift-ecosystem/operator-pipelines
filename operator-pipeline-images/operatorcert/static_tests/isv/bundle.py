0ce4aac217351c557d808832a4b88ba2c10d49b5
Ales Raszka <araszka@redhat.com>
2024-01-12 12:47:36 +0100

    Turn annotations validation to static check
    The annotations-validation tekton task migrated to a static check. The
    static check brings several benefits:
     - testability
     - reporting results to PR
     - simplification of a pipeline
    
    JIRA: ISV-4392
    
    Signed-off-by: Ales Raszka <araszka@redhat.com>

--- operator-pipeline-images/operatorcert/static_tests/isv/bundle.py
+++ operator-pipeline-images/operatorcert/static_tests/isv/bundle.py
@@ -3,7 +3,7 @@
 from collections.abc import Iterator
 
 from operator_repo import Bundle
-from operator_repo.checks import CheckResult, Warn
+from operator_repo.checks import CheckResult, Fail, Warn
 
 PRUNED_GRAPH_ERROR = (
     "olm.skipRange annotation is set but replaces field is not set in the CSV. "
@@ -38,3 +38,65 @@ def check_pruned_graph(bundle: Bundle) -> Iterator[CheckResult]:
                 # TODO: This needs to be changed to Fail when the check
                 # is publicly communicated
                 yield Warn(PRUNED_GRAPH_ERROR)
+
+
+def check_marketplace_annotation(bundle: Bundle) -> Iterator[CheckResult]:
+    """
+    Check if the bundle has the marketplace annotation set
+
+    Args:
+        bundle (Bundle): Tested operator bundle
+    """
+    repo = bundle.operator.repo
+    organization = repo.config.get("organization")
+    if organization != "redhat-marketplace":
+        return
+
+    csv_annotations = bundle.csv.get("metadata", {}).get("annotations", {})
+    expected_remote_workflow = (
+        "https://marketplace.redhat.com/en-us/operators/"
+        f"{bundle.csv_operator_name}/pricing?utm_source=openshift_console"
+    )
+    expected_support_workflow = (
+        "https://marketplace.redhat.com/en-us/operators/"
+        f"{bundle.csv_operator_name}/support?utm_source=openshift_console"
+    )
+
+    remote_workflow = csv_annotations.get("marketplace.openshift.io/remote-workflow")
+    support_workflow = csv_annotations.get("marketplace.openshift.io/support-workflow")
+
+    if remote_workflow != expected_remote_workflow:
+        yield Fail(
+            "CSV marketplace.openshift.io/remote-workflow annotation is set to "
+            f"'{remote_workflow}'. "
+            f"Expected value is {expected_remote_workflow}. "
+            "To fix this issue define the annotation in "
+            "'manifests/*.clusterserviceversion.yaml' file."
+        )
+    if support_workflow != expected_support_workflow:
+        yield Fail(
+            "CSV marketplace.openshift.io/support-workflow annotation is set to "
+            f"'{support_workflow}'. "
+            f"Expected value is {expected_support_workflow}. "
+            "To fix this issue define the annotation in "
+            "'manifests/*.clusterserviceversion.yaml' file."
+        )
+
+
+def check_operator_name(bundle: Bundle) -> Iterator[CheckResult]:
+    """
+    Ensure that the operator name matches the CSV name
+
+    Args:
+        bundle (Bundle): Tested operator bundle
+    """
+    annotation_package = bundle.annotations.get(
+        "operators.operatorframework.io.bundle.package.v1"
+    )
+    if annotation_package != bundle.csv_operator_name:
+        yield Warn(
+            f"Bundle package annotation is set to '{annotation_package}'. "
+            f"Expected value is '{bundle.csv_operator_name}' based on the CSV name. "
+            "To fix this issue define the annotation in "
+            "'metadata/annotations.yaml' file that matches the CSV name."
+        )
