983e7351eef867ea0a0e1dc2cd2cf5fa2b911a8a
haripate <haripate@redhat.com>
2024-01-19 12:54:37 -0500

    Fix up the regex.

--- operator-pipeline-images/operatorcert/static_tests/community/bundle.py
+++ operator-pipeline-images/operatorcert/static_tests/community/bundle.py
@@ -175,7 +175,9 @@ def check_required_fields(bundle: Bundle) -> Iterator[CheckResult]:
         ("metadata.annotations.categories", validate_categories, False),
         (
             "metadata.annotations.containerImage",
-            re.compile(r"([^/]+/){2,}[^/:]+:.+"),
+            re.compile(
+                r"[a-z0-9]+((\.|_|__|-+)[a-z0-9]+)*(\/[a-z0-9]+((\.|_|__|-+)[a-z0-9]+)*)*"
+            ),
             False,
         ),
         ("metadata.annotations.createdAt", validate_timestamp, True),
