059586cdc3ee6276a31dcf35b8c7b4f1a6c3cab8
Ales Raszka <araszka@redhat.com>
2024-01-26 13:22:32 +0100

    Add publishing entrypoint
    The previous commit was missing an entrypoint for publishing data in
    Pyxis.
    
    JIRA: ISV-3989
    
    Signed-off-by: Ales Raszka <araszka@redhat.com>

---
+++ operator-pipeline-images/operatorcert/entrypoints/publish_pyxis_image.py
@@ -0,0 +1,77 @@
+"""Publish image and related collections in Pyxis"""
+import argparse
+import logging
+import sys
+from typing import Any
+
+from operatorcert import pyxis
+from operatorcert.logger import setup_logger
+
+LOGGER = logging.getLogger("isv-sre-tools")
+
+
+def setup_argparser() -> Any:
+    """
+    Setup argument parser for this script.
+    """
+    parser = argparse.ArgumentParser(description="Container image publisher.")
+
+    parser.add_argument(
+        "--image-identifier",
+        help="Image _id to submit a new request for.",
+    )
+    parser.add_argument(
+        "--cert-project-id",
+        help="Certification project ID.",
+    )
+    parser.add_argument(
+        "--pyxis-url",
+        default="https://pyxis.engineering.redhat.com",
+        help="Base URL for Pyxis container metadata API",
+    )
+    parser.add_argument("--verbose", action="store_true", help="Verbose output")
+
+    return parser
+
+
+def submit_image_request(args: Any) -> Any:
+    """
+    Submit a new publish image request and wait for it to finish.
+
+    Args:
+        args (Any): CLI arguments
+    """
+
+    request = pyxis.post_image_request(
+        args.pyxis_url,
+        args.cert_project_id,
+        args.image_identifier,
+        "publish",
+    )
+    image_request = pyxis.wait_for_image_request(args.pyxis_url, request["_id"])
+    if image_request["status"] != "completed":
+        LOGGER.error(
+            "Image request failed: %s - %s",
+            image_request["status"],
+            image_request["status_message"],
+        )
+        sys.exit(1)
+    return image_request
+
+
+def main() -> None:
+    """
+    Main function
+    """
+    parser = setup_argparser()
+    args = parser.parse_args()
+
+    log_level = "INFO"
+    if args.verbose:
+        log_level = "DEBUG"
+    setup_logger(level=log_level)
+    submit_image_request(args)
+
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
---
+++ operator-pipeline-images/tests/entrypoints/test_publish_pyxis_image.py
@@ -0,0 +1,78 @@
+import json
+from datetime import datetime
+import pytest
+from unittest.mock import MagicMock, patch
+
+import operatorcert.entrypoints.publish_pyxis_image as publish_pyxis_image
+
+
+def test_setup_argparser() -> None:
+    parser = publish_pyxis_image.setup_argparser()
+
+    assert parser
+
+
+@patch("operatorcert.entrypoints.publish_pyxis_image.pyxis.wait_for_image_request")
+@patch("operatorcert.entrypoints.publish_pyxis_image.pyxis.post_image_request")
+def test_submit_image_request(
+    mock_post_image_request: MagicMock, mock_wait_for_image_request: MagicMock
+) -> None:
+    args = MagicMock()
+    args.pyxis_url = "https://catalog.redhat.com/api/containers/"
+    args.cert_project_id = "project_id"
+    args.image_identifier = "image_id"
+
+    mock_post_image_request.return_value = {"_id": "123"}
+    mock_wait_for_image_request.return_value = {"status": "completed", "_id": "123"}
+
+    result = publish_pyxis_image.submit_image_request(args)
+
+    assert result == mock_wait_for_image_request.return_value
+
+    mock_post_image_request.assert_called_with(
+        "https://catalog.redhat.com/api/containers/",
+        "project_id",
+        "image_id",
+        "publish",
+    )
+
+    mock_wait_for_image_request.assert_called_with(
+        "https://catalog.redhat.com/api/containers/",
+        mock_post_image_request.return_value["_id"],
+    )
+
+
+@patch("operatorcert.entrypoints.publish_pyxis_image.pyxis.wait_for_image_request")
+@patch("operatorcert.entrypoints.publish_pyxis_image.pyxis.post_image_request")
+def test_submit_image_request_error(
+    mock_post_image_request: MagicMock, mock_wait_for_image_request: MagicMock
+) -> None:
+    args = MagicMock()
+    args.pyxis_url = "https://catalog.redhat.com/api/containers/"
+    args.cert_project_id = "project_id"
+    args.image_identifier = "image_id"
+
+    mock_post_image_request.return_value = {"_id": "123"}
+    mock_wait_for_image_request.return_value = {
+        "status": "failed",
+        "_id": "123",
+        "status_message": "message  ",
+    }
+
+    with pytest.raises(SystemExit):
+        publish_pyxis_image.submit_image_request(args)
+
+
+@patch("operatorcert.entrypoints.publish_pyxis_image.submit_image_request")
+@patch("operatorcert.entrypoints.publish_pyxis_image.setup_logger")
+@patch("operatorcert.entrypoints.publish_pyxis_image.setup_argparser")
+def test_main(
+    mock_setup_argparser: MagicMock,
+    mock_setup_logger: MagicMock,
+    mock_submit_image_request: MagicMock,
+) -> None:
+    publish_pyxis_image.main()
+
+    mock_setup_argparser.assert_called_once()
+    mock_setup_logger.assert_called_once()
+    mock_submit_image_request.assert_called_once()
