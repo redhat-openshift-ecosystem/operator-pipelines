330bc319b030b6188ca5d20f222facb88f6e0377
Ales Raszka <araszka@redhat.com>
2024-02-12 12:17:55 +0100

    Detect changes in catalogs directory
    Changes in the catalogs directory are detected by the python script and
    returned as a json file. This commit extends existing script and looks
    for a new place in repository directory structure.
    
    JIRA: ISV-4491
    
    Signed-off-by: Ales Raszka <araszka@redhat.com>

--- operator-pipeline-images/operatorcert/entrypoints/detect_changed_operators.py
+++ operator-pipeline-images/operatorcert/entrypoints/detect_changed_operators.py
@@ -86,37 +86,47 @@ def github_pr_affected_files(pr_url: str) -> set[str]:
     return {x.filename for x in gh_pr.get_files()}
 
 
-def _find_owner(path: str) -> tuple[Optional[str], Optional[str]]:
+def _find_directory_owner(
+    path: str,
+) -> tuple[Optional[str], Optional[str], Optional[str], Optional[str]]:
     """
-    Given a relative file name within an operator repo, return a pair
-    (operator_name, bundle_version) indicating what bundle the file belongs to.
-    If the file is outside a bundle directory, the returned bundle_version will
-    be None.
-    If the file is outside an operator directory, both operator_name and
-    bundle_version will be None.
+    Given a relative file name within an operator repo, return a tuple
+    (operator_name, bundle_version, catalog, catalog_operator) indicating what
+    bundle and operator catalog the file belongs to.
+    If the file is outside a bundle or catalog directory, the returned
+    bundle_version or catalog_operator will be None.
+    If the file is outside an operator or catalog directory, all returns will be None.
     Args:
         path: Path to the file
     Returns:
-        (operator_name, bundle_version) Operator/bundle the files belongs to
+        (operator_name, bundle_version, catalog_name, catalog_operator)
+        Operator/bundle, Catalog/operator the files belongs to
     """
+
     # split the relative filename into a tuple of individual components
     filename_parts = pathlib.Path(path).parts
-    if len(filename_parts) < 3 or filename_parts[0] != "operators":
-        # path is outside an operator directory
-        return None, None
-    _, detected_operator_name, detected_bundle_version, *rest = filename_parts
-    if len(rest) < 1:
-        # inside an operator but not in a bundle (i.e.: the ci.yaml file)
-        return detected_operator_name, None
-    return detected_operator_name, detected_bundle_version
+    if len(filename_parts) >= 3 and filename_parts[0] == "operators":
+        # inside an operator directory
+        _, detected_operator_name, detected_bundle_version, *rest = filename_parts
+        if len(rest) < 1:
+            # inside an operator but not in a bundle (i.e.: the ci.yaml file)
+            return detected_operator_name, None, None, None
+        return detected_operator_name, detected_bundle_version, None, None
+    if len(filename_parts) >= 3 and filename_parts[0] == "catalogs":
+        # inside a catalog directory
+        _, detected_catalog_name, catalog_operator, *rest = filename_parts
+        if len(rest) >= 1:
+            return None, None, detected_catalog_name, catalog_operator
+    # path is outside an operator or catalog directory
+    return None, None, None, None
 
 
-def _affected_bundles_from_files(
+def _affected_bundles_and_operators_from_files(
     affected_files: set[str],
-) -> tuple[dict[str, set[Optional[str]]], set[str]]:
+) -> tuple[dict[str, set[Optional[str]]], dict[str, set[Optional[str]]], set[str]]:
     """
-    Group a given set of file names depending on the operator and bundle they
-    belong to
+    Group a given set of file names depending on the operator, bundle,
+    catalog and operator catalog they belong to
 
     Args:
         affected_files: set of files names to parse
@@ -126,26 +136,41 @@ def _affected_bundles_from_files(
         - a dictionary mapping operator names to the set of bundles affected
           (or None if there are files for that operator that do not belong
           to a bundle)
-        - a set of file names that do not belong to an operator
+        - a dictionary mapping catalog names to the set of operator affected
+          (or None if there are files for that operator that do not belong
+          to a bundle)
+        - a set of file names that do not belong to an operator or catalog
     """
-    non_operator_files: set[str] = set()
+    extra_files: set[str] = set()
     all_affected_bundles: dict[str, set[Optional[str]]] = {}
+    all_affected_catalog_operators: dict[str, set[Optional[str]]] = {}
 
     for filename in affected_files:
-        operator_name, bundle_version = _find_owner(filename)
-        if operator_name is None:
-            non_operator_files.add(filename)
-        else:
+        (
+            operator_name,
+            bundle_version,
+            catalog_name,
+            catalog_operator,
+        ) = _find_directory_owner(filename)
+        if operator_name is None and catalog_name is None:
+            extra_files.add(filename)
+        elif operator_name is not None:
             all_affected_bundles.setdefault(operator_name, set()).add(
                 # This is None for files within an operator but outside
                 # a bundle (i.e.: ci.yaml)
                 bundle_version
             )
-    return all_affected_bundles, non_operator_files
+        elif catalog_name is not None:
+            all_affected_catalog_operators.setdefault(catalog_name, set()).add(
+                catalog_operator
+            )
+    return all_affected_bundles, all_affected_catalog_operators, extra_files
 
 
 AffectedBundle = tuple[str, str]
 AffectedOperator = str
+AffectedCatalog = str
+AffectedCatalogOperator = tuple[str, str]
 
 
 @dataclass
@@ -176,63 +201,53 @@ class AffectedOperatorCollection:
         return self.added | self.modified | self.deleted
 
 
+@dataclass
+class AffectedCatalogOperatorCollection:
+    """A collection of affected bundles"""
+
+    added: set[AffectedCatalogOperator] = field(default_factory=set)
+    modified: set[AffectedCatalogOperator] = field(default_factory=set)
+    deleted: set[AffectedCatalogOperator] = field(default_factory=set)
+
+    @property
+    def union(self) -> set[AffectedCatalogOperator]:
+        """All the affected bundles"""
+        return self.added | self.modified | self.deleted
+
+
+@dataclass
+class AffectedCatalogCollection:
+    """A collection of affected operators"""
+
+    added: set[AffectedCatalog] = field(default_factory=set)
+    modified: set[AffectedCatalog] = field(default_factory=set)
+    deleted: set[AffectedCatalog] = field(default_factory=set)
+
+    @property
+    def union(self) -> set[AffectedCatalog]:
+        """All the affected operators"""
+        return self.added | self.modified | self.deleted
+
+
 def detect_changed_operators(
-    repo_path: pathlib.Path,
-    base_path: pathlib.Path,
-    pr_url: str,
+    head_repo: OperatorRepo,
+    base_repo: OperatorRepo,
+    all_affected_bundles: dict[str, set[Optional[str]]],
 ) -> dict[str, list[str]]:
     """
     Given two snapshots of an operator repository returns a dictionary
-    containing information on what operators and bundles have changed and how.
+    of changed operators
+
     Args:
-        repo_path: Path to the root of the snapshot of the operator repository
-            after the changes
-        base_path: Path to the root of the snapshot of the operator repository
-            before the changes
-        pr_url: URL of the corresponding GitHub pull request
+        head_repo (OperatorRepo): A snapshot of the operator repository after the changes
+        base_repo (OperatorRepo): A snapshot of the operator repository before the changes
+        all_affected_bundles (dict[str, set[Optional[str]]]): A dictionary mapping operator
 
     Returns:
-        A dictionary containing the following fields:
-            - extra_files: list of the names of the files that were affected
-                by the change but do not belong to an operator or a bundle
-            - affected_operators: list of the names of all operators affected
-                by the changes
-            - added_operators: list of the names of new operators that were
-                added by the changes
-            - modified_operators: list of the names of existing operators that
-                were modified by the changes
-            - deleted_operators: list of the names of operators that were
-                removed by the changes
-            - affected_bundles: list of the names of all bundles affected by
-                the changes
-            - added_bundles: list of the names of new bundles that were added
-                by the changes
-            - modified_bundles: list of the names of existing bundles that
-                were modified by the changes
-            - deleted_bundles: list of the names of bundles that were deleted
-                by the changes
+        dict[str, list[str]]: A dictionary with added, modified and deleted operators
     """
-
-    LOGGER.debug("Detecting changes in repo at %s", repo_path)
-
-    # Phase 1: using the diff between PR head and base, detect which bundles
-    # have been affected by the PR
-
-    all_affected_bundles, non_operator_files = _affected_bundles_from_files(
-        github_pr_affected_files(pr_url)
-    )
-
-    LOGGER.debug("Affected bundles: %s", all_affected_bundles)
-    LOGGER.debug("Files not belonging to an operator: %s", non_operator_files)
-
-    # Phase 2: Now that we know which operators and bundles have changed, determine exactly
-    # what was added, modified or removed
-
+    # determine exactly what was added, modified or removed
     affected_operators = AffectedOperatorCollection()
-    affected_bundles = AffectedBundleCollection()
-
-    head_repo = OperatorRepo(repo_path)
-    base_repo = OperatorRepo(base_path)
 
     affected_operators.added = {
         operator
@@ -249,6 +264,36 @@ def detect_changed_operators(
         for operator in all_affected_bundles
         if not head_repo.has(operator) and base_repo.has(operator)
     }
+
+    LOGGER.debug("Affected operators: %s", affected_operators)
+
+    return {
+        "affected_operators": list(affected_operators.union),
+        "added_operators": list(affected_operators.added),
+        "modified_operators": list(affected_operators.modified),
+        "deleted_operators": list(affected_operators.deleted),
+    }
+
+
+def detect_changed_operator_bundles(
+    head_repo: OperatorRepo,
+    base_repo: OperatorRepo,
+    all_affected_bundles: dict[str, set[Optional[str]]],
+) -> dict[str, list[str]]:
+    """
+    Given two snapshots of an operator repository returns a dictionary
+    of changed operator bundles
+
+    Args:
+        head_repo (OperatorRepo): A snapshot of the operator repository after the changes
+        base_repo (OperatorRepo): A snapshot of the operator repository before the changes
+        all_affected_bundles (dict[str, set[Optional[str]]]): A dictionary mapping operator
+
+    Returns:
+        dict[str, list[str]]: A dictionary with added, modified and deleted operator bundles
+    """
+    # determine exactly what was added, modified or removed
+    affected_bundles = AffectedBundleCollection()
 
     non_null_bundles = {
         (operator, bundle)
@@ -282,20 +327,177 @@ def detect_changed_operators(
         )
     }
 
-    LOGGER.debug("Affected operators: %s", affected_operators)
     LOGGER.debug("Affected bundles: %s", affected_bundles)
 
     return {
-        "extra_files": list(non_operator_files),
-        "affected_operators": list(affected_operators.union),
-        "added_operators": list(affected_operators.added),
-        "modified_operators": list(affected_operators.modified),
-        "deleted_operators": list(affected_operators.deleted),
         "affected_bundles": [f"{x}/{y}" for x, y in affected_bundles.union],
         "added_bundles": [f"{x}/{y}" for x, y in affected_bundles.added],
         "modified_bundles": [f"{x}/{y}" for x, y in affected_bundles.modified],
         "deleted_bundles": [f"{x}/{y}" for x, y in affected_bundles.deleted],
     }
+
+
+def detect_changed_catalog_operators(
+    head_repo: OperatorRepo,
+    base_repo: OperatorRepo,
+    all_affected_catalog_operators: dict[str, set[Optional[str]]],
+) -> dict[str, list[str]]:
+    """
+    Given two snapshots of an operator repository returns a dictionary
+    of changed catalog operators
+
+    Args:
+        head_repo (OperatorRepo): A snapshot of the operator repository after the changes
+        base_repo (OperatorRepo): A snapshot of the operator repository before the changes
+        all_affected_bundles (dict[str, set[Optional[str]]]): A dictionary mapping catalogs
+
+    Returns:
+        dict[str, list[str]]: A dictionary with added, modified and deleted
+        catalog operators
+    """
+    affected_catalog_operators = AffectedCatalogOperatorCollection()
+
+    non_null_bundles = {
+        (operator, bundle)
+        for operator, bundles in all_affected_catalog_operators.items()
+        for bundle in bundles
+        if bundle is not None
+    }
+
+    affected_catalog_operators.added = {
+        (catalog, operator_catalog)
+        for catalog, operator_catalog in non_null_bundles
+        if head_repo.has_catalog(catalog)
+        and head_repo.catalog(catalog).has(operator_catalog)
+        and not (
+            base_repo.has_catalog(catalog)
+            and base_repo.catalog(catalog).has(operator_catalog)
+        )
+    }
+
+    affected_catalog_operators.modified = {
+        (catalog, operator_catalog)
+        for catalog, operator_catalog in non_null_bundles
+        if head_repo.has_catalog(catalog)
+        and base_repo.has_catalog(catalog)
+        and head_repo.catalog(catalog).has(operator_catalog)
+        and base_repo.catalog(catalog).has(operator_catalog)
+    }
+
+    affected_catalog_operators.deleted = {
+        (catalog, operator_catalog)
+        for catalog, operator_catalog in non_null_bundles
+        if (not head_repo.has_catalog(catalog) and base_repo.has_catalog(catalog))
+        or (
+            not head_repo.catalog(catalog).has(operator_catalog)
+            and base_repo.catalog(catalog).has(operator_catalog)
+        )
+    }
+    LOGGER.debug("Affected catalog operators: %s", affected_catalog_operators)
+    return {
+        "affected_catalog_operators": [
+            f"{x}/{y}" for x, y in affected_catalog_operators.union
+        ],
+        "added_catalog_operators": [
+            f"{x}/{y}" for x, y in affected_catalog_operators.added
+        ],
+        "modified_catalog_operators": [
+            f"{x}/{y}" for x, y in affected_catalog_operators.modified
+        ],
+        "deleted_catalog_operators": [
+            f"{x}/{y}" for x, y in affected_catalog_operators.deleted
+        ],
+    }
+
+
+def detect_changed_catalogs(
+    head_repo: OperatorRepo,
+    base_repo: OperatorRepo,
+    all_affected_catalog_operators: dict[str, set[Optional[str]]],
+) -> dict[str, list[str]]:
+    """
+    Given two snapshots of an operator repository returns a dictionary
+    of changed catalogs
+
+    Args:
+        head_repo (OperatorRepo): A snapshot of the operator repository after the changes
+        base_repo (OperatorRepo): A snapshot of the operator repository before the changes
+        all_affected_bundles (dict[str, set[Optional[str]]]): A dictionary mapping catalogs
+
+    Returns:
+        dict[str, list[str]]: A dictionary with added, modified and deleted
+        catalogs
+    """
+    affected_catalogs = AffectedCatalogCollection()
+
+    affected_catalogs.added = {
+        catalog
+        for catalog in all_affected_catalog_operators
+        if head_repo.has_catalog(catalog) and not base_repo.has_catalog(catalog)
+    }
+
+    affected_catalogs.modified = {
+        catalog
+        for catalog in all_affected_catalog_operators
+        if head_repo.has_catalog(catalog) and base_repo.has_catalog(catalog)
+    }
+    affected_catalogs.deleted = {
+        catalog
+        for catalog in all_affected_catalog_operators
+        if not head_repo.has_catalog(catalog) and base_repo.has_catalog(catalog)
+    }
+
+    LOGGER.debug("Affected catalogs: %s", affected_catalogs)
+    return {
+        "affected_catalogs": list(affected_catalogs.union),
+        "added_catalogs": list(affected_catalogs.added),
+        "modified_catalogs": list(affected_catalogs.modified),
+        "deleted_catalogs": list(affected_catalogs.deleted),
+    }
+
+
+def detect_changes(
+    head_repo: OperatorRepo, base_repo: OperatorRepo, pr_url: str
+) -> dict[str, list[str]]:
+    """
+    Detect changes in the operator repository and return a dictionary containing
+    categories of changes on catalog, operator, bundle and
+    catalog operator level
+
+    Args:
+        head_repo (OperatorRepo): A snapshot of the operator repository after the changes
+        base_repo (OperatorRepo): A snapshot of the operator repository before the changes
+        pr_url (str): Pull request URL with the changes
+
+    Returns:
+        dict[str, list[str]]: Changes in the operator repository
+    """
+    pr_files = github_pr_affected_files(pr_url)
+    (
+        all_affected_bundles,
+        all_affected_catalog_operators,
+        non_operator_files,
+    ) = _affected_bundles_and_operators_from_files(pr_files)
+
+    operators = detect_changed_operators(head_repo, base_repo, all_affected_bundles)
+    bundles = detect_changed_operator_bundles(
+        head_repo, base_repo, all_affected_bundles
+    )
+    catalogs = detect_changed_catalogs(
+        head_repo, base_repo, all_affected_catalog_operators
+    )
+    catalog_operators = detect_changed_catalog_operators(
+        head_repo, base_repo, all_affected_catalog_operators
+    )
+
+    result = {
+        **operators,
+        **bundles,
+        **catalogs,
+        **catalog_operators,
+        "extra_files": list(non_operator_files),
+    }
+    return result
 
 
 def main() -> None:
@@ -313,11 +515,10 @@ def main() -> None:
     setup_logger(level=log_level)
 
     # Logic
-    result = detect_changed_operators(
-        args.repo_path,
-        args.base_repo_path,
-        args.pr_url,
-    )
+    head_repo = OperatorRepo(args.repo_path)
+    base_repo = OperatorRepo(args.base_repo_path)
+
+    result = detect_changes(head_repo, base_repo, args.pr_url)
 
     if args.output_file:
         with args.output_file.open(mode="w", encoding="utf-8") as output_file:
