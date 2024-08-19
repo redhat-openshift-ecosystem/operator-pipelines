46bcba81e97272381aac8614936646b486174884
Martin Jediny <jedinym@proton.me>
2024-08-19 11:21:31 +0200

    [ISV-4964] Implement Preflight version invalidation tool (#694)
    * [ISV-4964] Implement Preflight version invalidation tool
    
    * [ISV-4964] Fixup tests
    
    * [ISV-4964] Ignore log branch in coverage
    
    * [ISV-4964] Add missing types
    
    * [ISV-4964] Require auth for Pyxis read
    
    * [ISV-4964] Create preflight invalidation deployment playbook
    
    * [ISV-4964] Add deployment example
    
    * [ISV-4964] Remove debug message
    
    * [ISV-4964] Rename invalidation script
    
    * [ISV-4964] Use released image for preflight invalidation
    
    * [ISV-4964] Add preflight invalidation role to deployment playbook
    
    * [ISV-4964] Use in-API filter for versions

---
+++ operator-pipeline-images/operatorcert/entrypoints/invalidate_preflight_versions.py
@@ -0,0 +1,184 @@
+"""
+Tool automating invalidation of older versions of Preflight in Pyxis based on
+rules in https://issues.redhat.com/browse/ISV-4964.
+"""
+
+from datetime import datetime, timedelta
+from typing import Any
+import sys
+import argparse as ap
+from dataclasses import dataclass
+import urllib.parse
+import json
+import logging
+import pprint
+from itertools import islice
+from packaging.version import Version
+import requests
+
+from requests import Response
+
+from operatorcert import pyxis
+
+logger = logging.getLogger(__name__)
+
+
+@dataclass
+class PreflightVersion:
+    """Object representing a Preflight version in Pyxis."""
+
+    id: str
+    created: datetime
+    version: Version
+    enabled_for_testing: bool
+
+
+def parse_versions(data: dict[str, Any]) -> list[PreflightVersion]:
+    """Parse raw Pyxis dictionary into PreflightVersion object."""
+    versions = []
+    for version in data["data"]:
+        _id = version["_id"]
+        enabled = version["enabled_for_testing"]
+        created = datetime.fromisoformat(version["creation_date"])
+        version = Version(version["version"])
+        versions.append(
+            PreflightVersion(
+                id=_id, created=created, version=version, enabled_for_testing=enabled
+            )
+        )
+
+    return versions
+
+
+def get_version_data_page(url: str, page: int, page_size: int) -> bytes:
+    """Get single page of preflight data."""
+    preflight_filter = (
+        "name==github.com/redhat-openshift-ecosystem/openshift-preflight"
+        + ";enabled_for_testing==true"
+    )
+    params = {
+        "filter": preflight_filter,
+        "page": page,
+        "page_size": page_size,
+    }
+
+    resp: Response = pyxis.get(url, params)
+    if resp.status_code != 200:
+        logger.warning("Pyxis returned code %s: %s", resp.status_code, resp.content)
+        raise RuntimeError()
+
+    return resp.content
+
+
+def get_versions(pyxis_url: str, page_size: int = 100) -> list[PreflightVersion]:
+    """Get a list of current Preflight versions in Pyxis."""
+    url = urllib.parse.urljoin(pyxis_url, "v1/tools")
+
+    versions: list[PreflightVersion] = []
+
+    page = 0
+    data = json.loads(get_version_data_page(url, page, page_size))
+
+    versions.extend(parse_versions(data))
+
+    total = int(data["total"])
+    while len(versions) != total:
+        page += 1
+        data = json.loads(get_version_data_page(url, page, page_size))
+        versions.extend(parse_versions(data))
+        total = int(data["total"])
+
+    return versions
+
+
+def get_versions_to_disable(versions: list[PreflightVersion]) -> list[PreflightVersion]:
+    """Get Preflight versions to disable based on current versions in Pyxis."""
+    versions.sort(reverse=True, key=lambda v: v.version)
+
+    to_update = []
+
+    # ignore two newest versions
+    older_versions = islice(versions, 2, None)
+
+    for version in older_versions:
+        now = datetime.now(version.created.tzinfo)
+
+        if now - version.created > timedelta(days=90):
+            to_update.append(version)
+
+    return to_update
+
+
+def disable_version(pyxis_url: str, version: PreflightVersion) -> None:
+    """Disable a Preflight version in Pyxis."""
+    url = urllib.parse.urljoin(pyxis_url, f"v1/tools/id/{version.id}")
+    pyxis.patch(url, {"enabled_for_testing": False})
+
+
+def synchronize_versions(pyxis_url: str, dry_run: bool, log_current: bool) -> None:
+    """
+    Invalidate older versions of Preflight in Pyxis based on rules in
+    https://issues.redhat.com/browse/ISV-4964
+    """
+    current = get_versions(pyxis_url)
+    if log_current or dry_run:  # pragma: no cover
+        logger.info("Current versions in Pyxis: %s", pprint.pformat(current))
+    to_disable = get_versions_to_disable(current)
+
+    if dry_run:  # pragma: no cover
+        logger.info(
+            "Versions to be disabled: %s",
+            pprint.pformat([(v.id, v.version) for v in to_disable]),
+        )
+        return
+
+    for version in to_disable:
+        disable_version(pyxis_url, version)
+        logger.info(
+            "Disabled version: %s", pprint.pformat((version.id, version.version))
+        )
+
+
+def main() -> int:  # pragma: no cover
+    """
+    Invalidate older versions of Preflight in Pyxis based on rules in
+    https://issues.redhat.com/browse/ISV-4964
+    """
+    logging.basicConfig(level=logging.DEBUG)
+    parser = ap.ArgumentParser(
+        description="Tool automating invalidation of older Preflight versions in Pyxis. "
+        "https://issues.redhat.com/browse/ISV-4964"
+    )
+    parser.add_argument(
+        "-d",
+        "--dry-run",
+        action="store_true",
+        help="print versions to be disabled and exit",
+    )
+    parser.add_argument(
+        "-c", "--log-current", action="store_true", help="log current versions in Pyxis"
+    )
+    parser.add_argument(
+        "--pyxis-url",
+        default="https://pyxis.engineering.redhat.com/",
+        help="base URL for Pyxis container metadata API",
+    )
+    args = parser.parse_args()
+
+    retries = 5
+    while retries > 0:
+        try:
+            synchronize_versions(args.pyxis_url, args.dry_run, args.log_current)
+            break
+        except (requests.HTTPError, RuntimeError):
+            retries -= 1
+
+    if retries == 0:
+        logger.error("Failed to update preflight versions")
+        return 1
+
+    return 0
+
+
+if __name__ == "__main__":  # pragma: no cover
+    sys.exit(main())
---
+++ operator-pipeline-images/tests/entrypoints/test_invalidate_preflight_versions.py
@@ -0,0 +1,232 @@
+import json
+from unittest.mock import patch, MagicMock
+import pytest
+from typing import Any
+import datetime
+
+from operatorcert.entrypoints.invalidate_preflight_versions import (
+    synchronize_versions,
+    get_versions,
+)
+
+PYXIS_URL = "https://pyxis.com"
+
+
+@patch("operatorcert.entrypoints.invalidate_preflight_versions.pyxis.patch")
+@patch("operatorcert.entrypoints.invalidate_preflight_versions.pyxis.get")
+@pytest.mark.parametrize(
+    "content",
+    [
+        {
+            "data": [
+                {
+                    "_id": "N-1",
+                    "creation_date": "2024-06-27T17:53:39.153000+00:00",
+                    "enabled_for_testing": True,
+                    "version": "1.9.9",
+                },
+                {
+                    "_id": "N",
+                    "creation_date": "2024-07-15T15:26:51.408000+00:00",
+                    "enabled_for_testing": True,
+                    "version": "1.10.0",
+                },
+            ],
+            "total": 2,
+        },
+        {
+            "data": [
+                {
+                    "_id": "N-2",
+                    "creation_date": "2024-06-27T17:53:39.153000+00:00",
+                    "enabled_for_testing": True,
+                    "version": "1.9.8",
+                },
+                {
+                    "_id": "N-1",
+                    "creation_date": "2024-06-27T17:53:39.153000+00:00",
+                    "enabled_for_testing": True,
+                    "version": "1.9.9",
+                },
+                {
+                    "_id": "N",
+                    "creation_date": "2024-07-15T15:26:51.408000+00:00",
+                    "enabled_for_testing": True,
+                    "version": "1.10.0",
+                },
+            ],
+            "total": 3,
+        },
+    ],
+    ids=["only-two", "valid-time"],
+)
+def test_no_change(
+    mock_get: MagicMock,
+    mock_patch: MagicMock,
+    content: dict[str, Any],
+) -> None:
+    mock_resp = MagicMock()
+    mock_resp.status_code = 200
+    mock_resp.content = json.dumps(content)
+
+    mock_get.return_value = mock_resp
+
+    with patch(
+        "operatorcert.entrypoints.invalidate_preflight_versions.datetime"
+    ) as mock_date:
+        # have to patch fromisoformat as well or the test will break
+        mock_date.fromisoformat = datetime.datetime.fromisoformat
+
+        mock_date.now.return_value = datetime.datetime.fromisoformat(
+            "2024-07-17T14:44:54.546896+00:00"
+        )
+
+        synchronize_versions(pyxis_url=PYXIS_URL, dry_run=False, log_current=False)
+
+    mock_patch.assert_not_called()
+
+
+@patch("operatorcert.entrypoints.invalidate_preflight_versions.pyxis.patch")
+@patch("operatorcert.entrypoints.invalidate_preflight_versions.pyxis.get")
+@pytest.mark.parametrize(
+    ["content", "expected_patch_urls"],
+    [
+        (
+            {
+                "data": [
+                    {
+                        "_id": "N-2",
+                        "creation_date": "2023-06-27T17:53:39.153000+00:00",
+                        "enabled_for_testing": True,
+                        "version": "1.9.8",
+                    },
+                    {
+                        "_id": "N-1",
+                        "creation_date": "2024-06-27T17:53:39.153000+00:00",
+                        "enabled_for_testing": True,
+                        "version": "1.9.9",
+                    },
+                    {
+                        "_id": "N",
+                        "creation_date": "2024-07-15T15:26:51.408000+00:00",
+                        "enabled_for_testing": True,
+                        "version": "1.10.0",
+                    },
+                ],
+                "total": 3,
+            },
+            [f"{PYXIS_URL}/v1/tools/id/N-2"],
+        ),
+        (
+            {
+                "data": [
+                    {
+                        "_id": "N-3",
+                        "creation_date": "2023-06-27T17:53:39.153000+00:00",
+                        "enabled_for_testing": True,
+                        "version": "1.9.7",
+                    },
+                    {
+                        "_id": "N-2",
+                        "creation_date": "2023-06-27T17:53:39.153000+00:00",
+                        "enabled_for_testing": True,
+                        "version": "1.9.8",
+                    },
+                    {
+                        "_id": "N-1",
+                        "creation_date": "2024-06-27T17:53:39.153000+00:00",
+                        "enabled_for_testing": True,
+                        "version": "1.9.9",
+                    },
+                    {
+                        "_id": "N",
+                        "creation_date": "2024-07-15T15:26:51.408000+00:00",
+                        "enabled_for_testing": True,
+                        "version": "1.10.0",
+                    },
+                ],
+                "total": 4,
+            },
+            [f"{PYXIS_URL}/v1/tools/id/N-2", f"{PYXIS_URL}/v1/tools/id/N-3"],
+        ),
+    ],
+    ids=["one-old-release", "two-old-releases"],
+)
+def test_disable_old(
+    mock_get: MagicMock,
+    mock_patch: MagicMock,
+    content: dict[str, Any],
+    expected_patch_urls: list[str],
+) -> None:
+    mock_resp = MagicMock()
+    mock_resp.status_code = 200
+    mock_resp.content = json.dumps(content)
+
+    mock_get.return_value = mock_resp
+
+    with patch(
+        "operatorcert.entrypoints.invalidate_preflight_versions.datetime"
+    ) as mock_date:
+        # have to patch fromisoformat as well or the test will break
+        mock_date.fromisoformat = datetime.datetime.fromisoformat
+
+        mock_date.now.return_value = datetime.datetime.fromisoformat(
+            "2024-07-17T14:44:54.546896+00:00"
+        )
+
+        synchronize_versions(pyxis_url=PYXIS_URL, dry_run=False, log_current=False)
+
+    for expected_url in expected_patch_urls:
+        mock_patch.assert_any_call(expected_url, {"enabled_for_testing": False})
+
+    assert mock_patch.call_count == len(expected_patch_urls)
+
+
+@patch("operatorcert.entrypoints.invalidate_preflight_versions.get_version_data_page")
+def test_get_version_paging(mock_data_page: MagicMock) -> None:
+    mock_data_page.side_effect = map(
+        json.dumps,
+        [
+            {
+                "data": [
+                    {
+                        "_id": "N-1",
+                        "creation_date": "2024-06-27T17:53:39.153000+00:00",
+                        "enabled_for_testing": True,
+                        "version": "1.9.9",
+                    },
+                    {
+                        "_id": "N",
+                        "creation_date": "2024-07-15T15:26:51.408000+00:00",
+                        "enabled_for_testing": True,
+                        "version": "1.10.0",
+                    },
+                ],
+                "total": 3,
+            },
+            {
+                "data": [
+                    {
+                        "_id": "N-2",
+                        "creation_date": "2024-06-27T17:53:39.153000+00:00",
+                        "enabled_for_testing": True,
+                        "version": "1.9.9",
+                    },
+                ],
+                "total": 3,
+            },
+        ],
+    )
+
+    versions = get_versions(PYXIS_URL, 2)
+    assert len(versions) == 3
+
+
+@patch("operatorcert.entrypoints.invalidate_preflight_versions.pyxis.get")
+def test_pyxis_error(mock_pyxis_get: MagicMock) -> None:
+    mock_resp = MagicMock()
+    mock_resp.status_code = 500
+    mock_pyxis_get.return_value = mock_resp
+
+    with pytest.raises(RuntimeError):
+        synchronize_versions(PYXIS_URL, dry_run=False, log_current=False)
