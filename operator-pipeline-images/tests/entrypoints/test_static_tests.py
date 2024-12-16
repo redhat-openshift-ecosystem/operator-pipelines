d169ca76a4ed2cdb52d9a8631b5164dbd96fc67e
Tomáš Man <36169139+mantomas@users.noreply.github.com>
2024-12-16 11:27:57 +0100

    [ISV-5446] static check for catalog bundle images (#753)
    * [ISV-5446] static check for catalog bundle images

--- operator-pipeline-images/tests/entrypoints/test_static_tests.py
+++ operator-pipeline-images/tests/entrypoints/test_static_tests.py
@@ -5,18 +5,20 @@ import pytest
 from operator_repo import Repo
 from operator_repo.checks import Fail, Warn
 from operatorcert.entrypoints import static_tests
-from tests.utils import bundle_files, create_files
+from tests.utils import bundle_files, catalog_files, create_files
 
 
 @pytest.mark.parametrize(
-    "check_results, expected",
+    "check_results, bundle, expected",
     [
         (
             [],
+            "0.0.1",
             {"passed": True, "outputs": []},
         ),
         (
             [Warn("foo")],
+            "0.0.1",
             {
                 "passed": True,
                 "outputs": [
@@ -26,6 +28,7 @@ from tests.utils import bundle_files, create_files
         ),
         (
             [Fail("bar", check="baz")],
+            "",
             {
                 "passed": False,
                 "outputs": [
@@ -40,6 +43,7 @@ from tests.utils import bundle_files, create_files
         ),
         (
             [Warn("foo"), Fail("bar")],
+            "0.0.1",
             {
                 "passed": False,
                 "outputs": [
@@ -58,40 +62,55 @@ from tests.utils import bundle_files, create_files
     ],
 )
 @patch("operatorcert.entrypoints.static_tests.run_suite")
-def test_check_bundle(
-    mock_run_suite: MagicMock, tmp_path: Any, check_results: Any, expected: Any
+def test_execute_checks(
+    mock_run_suite: MagicMock,
+    tmp_path: Any,
+    bundle: str,
+    check_results: Any,
+    expected: Any,
 ) -> None:
     operator_name = "test-operator"
-    bundle_version = "0.0.1"
-    create_files(tmp_path, bundle_files(operator_name, bundle_version))
+    bundle_version = bundle
+    affected_catalogs = "v4.14/test-operator"
+    create_files(
+        tmp_path,
+        bundle_files(operator_name, "0.0.1"),
+        catalog_files("v4.14", "test-operator"),
+    )
 
     repo = Repo(tmp_path)
 
     mock_run_suite.return_value = iter(check_results)
-    result = static_tests.check_bundle(
-        str(repo.root), operator_name, bundle_version, ["dummy_suite"]
+    result = static_tests.execute_checks(
+        str(repo.root),
+        operator_name,
+        bundle_version,
+        affected_catalogs,
+        ["dummy_suite"],
     )
     assert result == expected
 
 
-@patch("operatorcert.entrypoints.static_tests.check_bundle")
+@patch("operatorcert.entrypoints.static_tests.execute_checks")
 @patch("operatorcert.entrypoints.static_tests.setup_logger")
 def test_static_tests_main(
-    mock_logger: MagicMock, mock_check_bundle: MagicMock, capsys: Any, tmpdir: Any
+    mock_logger: MagicMock, mock_execute_checks: MagicMock, capsys: Any, tmpdir: Any
 ) -> None:
     args = [
         "static-tests",
         "--repo-path=/tmp/repo",
         "test-operator",
         "0.0.1",
+        ["v4.14/test-operator"],
     ]
-    mock_check_bundle.return_value = {"foo": ["bar"]}
+    mock_execute_checks.return_value = {"foo": ["bar"]}
     with patch("sys.argv", args):
         static_tests.main()
-    mock_check_bundle.assert_called_once_with(
+    mock_execute_checks.assert_called_once_with(
         "/tmp/repo",
         "test-operator",
         "0.0.1",
+        ["v4.14/test-operator"],
         ["operatorcert.static_tests.community", "operatorcert.static_tests.common"],
         [],
     )
@@ -99,7 +118,7 @@ def test_static_tests_main(
     mock_logger.assert_called_once_with(level="INFO")
 
     mock_logger.reset_mock()
-    mock_check_bundle.reset_mock()
+    mock_execute_checks.reset_mock()
 
     out_file = tmpdir / "out.json"
     out_file_name = str(out_file)
@@ -109,17 +128,19 @@ def test_static_tests_main(
         "--skip-tests=check_123,check_456",
         "other-test-operator",
         "0.0.2",
+        ["v4.14/test-operator"],
         "--suites=other_suite",
         f"--output-file={out_file_name}",
         "--verbose",
     ]
-    mock_check_bundle.return_value = {"bar": ["baz"]}
+    mock_execute_checks.return_value = {"bar": ["baz"]}
     with patch("sys.argv", args):
         static_tests.main()
-    mock_check_bundle.assert_called_once_with(
+    mock_execute_checks.assert_called_once_with(
         "/tmp/other_repo",
         "other-test-operator",
         "0.0.2",
+        ["v4.14/test-operator"],
         ["other_suite"],
         ["check_123", "check_456"],
     )
---
+++ operator-pipeline-images/tests/static_tests/common/test_operator_catalogs.py
@@ -0,0 +1,69 @@
+from pathlib import Path
+from typing import Any
+
+import pytest
+
+from operator_repo import OperatorCatalogList, Repo
+from operatorcert.static_tests.common.operator_catalogs import (
+    check_bundle_images_in_fbc,
+)
+from tests.utils import bundle_files, catalog_files, create_files
+
+
+@pytest.mark.parametrize(
+    ["config", "expected_results"],
+    [
+        (
+            {},
+            set(),
+        ),
+        (
+            {
+                "allowed_bundle_registries": ["quay.io/org-foo/", "quay.io/org-bar/"],
+            },
+            set(),
+        ),
+        (
+            {
+                "allowed_bundle_registries": ["quay.io/org-foo/"],
+            },
+            {
+                "Invalid bundle image(s) found in OperatorCatalog(v4.14/fake-operator): "
+                "quay.io/org-bar/registry/bundle@sha256:123. "
+                "Only these registries are allowed for bundle images: "
+                "quay.io/org-foo/.",
+            },
+        ),
+    ],
+    ids=[
+        "Config not set",
+        "All registries allowed",
+        "Registry not allowed",
+    ],
+)
+def test_check_bundle_images_in_fbc(
+    tmp_path: Path,
+    config: dict[str, Any],
+    expected_results: set[str],
+) -> None:
+    catalog_content = (
+        {
+            "schema": "olm.bundle",
+            "image": "quay.io/org-foo/registry/bundle@sha256:123",
+        },
+        {
+            "schema": "olm.bundle",
+            "image": "quay.io/org-bar/registry/bundle@sha256:123",
+        },
+    )
+    create_files(
+        tmp_path,
+        catalog_files("v4.14", "fake-operator", content=catalog_content),
+        bundle_files("fake-operator", "0.0.1"),
+        {"config.yaml": config},
+    )
+    repo = Repo(tmp_path)
+    catalog = repo.catalog("v4.14")
+    operator_catalog = catalog.operator_catalog("fake-operator")
+    catalogs = OperatorCatalogList([operator_catalog])
+    assert {x.reason for x in check_bundle_images_in_fbc(catalogs)} == expected_results
