eb93cefbb6a24021cdae50cea36dbfc54da2dd5d
Jakub Durkac <jdurkac@redhat.com>
2026-07-28 16:59:58 +0200

    feat(ISV-7155): revert "Temporary workaround to overwrite pending repos manually"
    This reverts commit d87bc949b14a71c5dec7dd3adc67059a9dc5e872.
    Signed-off-by: Jakub Durkac <jdurkac@redhat.com>

--- operatorcert/entrypoints/add_fbc_fragments_to_index.py
+++ operatorcert/entrypoints/add_fbc_fragments_to_index.py
@@ -204,11 +204,8 @@ def add_fbc_fragment_to_index(
             payload["build_tags"] = [version, f"{version}-{build_tags_suffix}"]
 
         if overwrite_token:
-            # WORKAROUND: Manually overwriting index images using skopeo
-            # TODO: uncomment when overwrite token is fixed, delete pass
-            # payload["overwrite_from_index"] = True
-            # payload["overwrite_from_index_token"] = overwrite_token
-            pass
+            payload["overwrite_from_index"] = True
+            payload["overwrite_from_index_token"] = overwrite_token
 
         resp = iib.add_fbc_build(iib_url, payload)
         request_ids.append(resp["id"])
